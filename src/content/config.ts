ce19c4481f1c047d7215dad7f125e9a3d3ee70dc
mukhriddin-dev <khodievm@gmail.com>
2024-09-01 03:00:52 +0500

    added english and arabic pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -27,13 +27,45 @@ const postSchema = z.object({
     size: z.number(), 
 });
 
+const englishPostSchema = z.object({
+    title: z.string(),
+    audioUrl: z.string(),
+    pubDate: z.coerce.date().optional(),
+    cover: z.string().optional(),
+    explicit: z.boolean().optional(),
+    episode: z.number().optional(),
+    season: z.number().optional(),
+    episodeType: z.string().optional(),
+    duration: z.coerce.string().optional(), 
+    size: z.number(), 
+});
+
+const arabicPostSchema = z.object({
+    title: z.string(),
+    audioUrl: z.string(),
+    pubDate: z.coerce.date().optional(),
+    cover: z.string().optional(),
+    explicit: z.boolean().optional(),
+    episode: z.number().optional(),
+    season: z.number().optional(),
+    episodeType: z.string().optional(),
+    duration: z.coerce.string().optional(), 
+    size: z.number(), 
+});
+
 export type episodeSchema = z.infer<typeof episodeSchema>;
 export type postSchema = z.infer<typeof postSchema>;
+export type englishPostSchema = z.infer<typeof englishPostSchema>;
+export type arabicPostSchema = z.infer<typeof arabicPostSchema>;
 
 const episodeCollection = defineCollection({ schema: episodeSchema });
 const postCollection = defineCollection({ schema: postSchema });
+const englishPostCollection = defineCollection({ schema: englishPostSchema });
+const arabicPostCollection = defineCollection({ schema: arabicPostSchema });
 
 export const collections = {
     'episode': episodeCollection,
-    'posts': postCollection
+    'posts': postCollection,
+    'english': englishPostCollection,
+    'arabic': arabicPostCollection,
 }
