d42393849ceee8e69c6293865f64b45910dd6d85
mukhriddin-dev <khodievm@gmail.com>
2024-09-17 12:08:32 +0500

    fixed:

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
-export const SITE_TITLE = 'OPEN BLOG | KHODIEFF.UZ';
-export const SITE_DESCRIPTION = 'OPEN BLOG | KHODIEFF.UZ'
+export const SITE_TITLE = "OPEN BLOG | KHODIEFF.UZ";
+export const SITE_DESCRIPTION = "OPEN BLOG | KHODIEFF.UZ";
+export { default as GoogleAnalytics } from "./components/GoogleAnalytics.astro";
